38f4bd85787b3687bc9a112f8b1102d955da0258
Viraj Rakholiya <virajrakholiya14@gmail.com>
2024-08-06 20:49:02 +0530

    ok

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center bg-black">
+    <div className="flex items-center justify-center bg-black h-screen">
       <SignIn />
     </div>
   );
